98b93aa892359cf15f46ece9c6164d7dcd9b3e9c
nakcomel <muutiaanita1@gmail.com>
2024-03-02 12:37:48 +0900

    semogabisa

--- lat1.py
+++ lat1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from page1 import page_1
 from page2 import page_2
+from page3 import page_3
 # import pandas as pd
 #import matplotlib.pyplot as plt
 
@@ -15,9 +16,6 @@ from page2 import page_2
 
 
 
-def page_3():
-    st.title("Halaman 3")
-    st.write('Halaman ini digunakan untuk Menampilkan Rumus Matematika')
     
 PAGES = {
     "Page 1" : page_1,
