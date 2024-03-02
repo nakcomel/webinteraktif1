50a7976056745701d067691383a80f29d2ff8226
nakcomel <muutiaanita1@gmail.com>
2024-03-02 11:09:03 +0900

    mutiacantik

--- lat1.py
+++ lat1.py
@@ -1,12 +1,31 @@
 import streamlit as st
-import pandas as pd
+# import pandas as pd
 #import matplotlib.pyplot as plt
 
-st.write("mutisa")
-df = pd.DataFrame({
-    'No': [1,2,3,4],
-    'Nama': ['ucup','acep','acip','acup'],
-    'nilai': [100, 100, 100, 100]
-})
+# st.write("mutisa")
+# df = pd.DataFrame({
+#     'No': [1,2,3,4],
+#     'Nama': ['ucup','acep','acip','acup'],
+#     'nilai': [100, 100, 100, 100]
+# })
 
-df
+# df
+
+def page_1():
+    st.title("Halaman 1")
+    st.write('Halaman ini digunakan untuk intro')
+def page_2():
+    st.title("Halaman 2")
+    st.write('Halaman ini digunakan untuk Menampilkan youtube')
+def page_3():
+    st.title("Halaman 3")
+    st.write('Halaman ini digunakan untuk Menampilkan Rumus Matematika')
+    
+PAGES = {
+    "Page 1" : page_1,
+    "Page 2" : page_2,
+    "Page 3" : page_3
+}
+
+page = st.sidebar.radio("Halaman", list(PAGES.keys()))
+PAGES[page]()
