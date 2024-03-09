21368c20b0d1d5146d2f8534496a063815b00a07
nakcomel <muutiaanita1@gmail.com>
2024-03-09 12:47:03 +0900

    hgjsdgjds

--- lat1.py
+++ lat1.py
@@ -40,6 +40,9 @@ st.markdown(
                 display: none;
             }
             
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            
             </style>
             """,
     unsafe_allow_html=True,
